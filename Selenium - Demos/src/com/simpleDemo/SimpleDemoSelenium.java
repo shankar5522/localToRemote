3d979c0b4fb13d8e187282bcdef795261bb80e29
shankarl <shankarl@DIN16004682.corp.capgemini.com>
2018-01-18 19:25:37 +0530

    firsr Demo push to Remote Repo

---
+++ Selenium - Demos/src/com/simpleDemo/SimpleDemoSelenium.java
@@ -0,0 +1,26 @@
+package com.simpleDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleDemoSelenium {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Users\\shankarl\\Downloads\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("http://newtours.demoaut.com/");
+		
+		Thread.sleep(2000);
+		driver.close();
+		
+		
+
+	}
+
+}
