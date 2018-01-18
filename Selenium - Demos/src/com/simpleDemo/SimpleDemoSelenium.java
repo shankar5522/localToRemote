4d80820cc1ae1bd946d4ba0f560f707f036f8179
shankarl <shankarl@DIN16004682.corp.capgemini.com>
2018-01-18 19:33:18 +0530

    shankar

--- Selenium - Demos/src/com/simpleDemo/SimpleDemoSelenium.java
+++ Selenium - Demos/src/com/simpleDemo/SimpleDemoSelenium.java
@@ -1,5 +1,7 @@
 package com.simpleDemo;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,6 +13,10 @@ public class SimpleDemoSelenium {
 		System.setProperty("webdriver.chrome.driver", "D:\\Users\\shankarl\\Downloads\\Selenium\\chromedriver.exe");
 		
 		WebDriver driver=new ChromeDriver();
+		
+		
+		//firtst commet with impl implecitywait
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		driver.manage().window().maximize();
 		
